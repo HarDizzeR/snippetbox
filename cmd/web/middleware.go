b34d94b9d7e2b7d1b003578d3b4c02d964b4183f
HarDizzeR <96432989+HarDizzeR@users.noreply.github.com>
2024-11-15 12:59:35 +0200

    Added about page, more authentication

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,39 +1,52 @@
 package main
 
 import (
-	"net/http"
+	"context"
 	"fmt"
+	"net/http"
+
+	"github.com/justinas/nosurf"
 )
 
+func noSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   true,
+	})
+	return csrfHandler
+}
+
 func commonHeaders(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-        w.Header().Set("Content-Security-Policy",
-            "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+		w.Header().Set("Content-Security-Policy",
+			"default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
 
-        w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
-        w.Header().Set("X-Content-Type-Options", "nosniff")
-        w.Header().Set("X-Frame-Options", "deny")
-        w.Header().Set("X-XSS-Protection", "0")
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "deny")
+		w.Header().Set("X-XSS-Protection", "0")
 
-        w.Header().Set("Server", "Go")
+		w.Header().Set("Server", "Go")
 
-        next.ServeHTTP(w, r)
-    })
+		next.ServeHTTP(w, r)
+	})
 }
 
 func (app *application) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request){
+		func(w http.ResponseWriter, r *http.Request) {
 			var (
-				ip = r.RemoteAddr
-				proto = r.Proto
+				ip     = r.RemoteAddr
+				proto  = r.Proto
 				method = r.Method
-				uri = r.URL.RequestURI()
+				uri    = r.URL.RequestURI()
 			)
 			app.logger.Info("received request", "ip", ip, "proto", proto, "method", method, "uri", uri)
 			next.ServeHTTP(w, r)
-	})
+		})
 }
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -48,5 +61,40 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 			}()
 
 			next.ServeHTTP(w, r)
+		})
+}
+
+func (app *application) requireAuthentcation(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			if !app.isAuthenticated(r) {
+				http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+				return
+			}
+			w.Header().Add("Cache-Control", "no-store")
+
+			next.ServeHTTP(w, r)
+		})
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := app.sessionManager.GetInt(r.Context(), "authenticatedUserID")
+		if id == 0 {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		exists, err := app.users.Exists(id)
+		if err != nil {
+			app.serverError(w, r, err)
+			return
+		}
+
+		if exists {
+			ctx := context.WithValue(r.Context(), isAuthenticatedContextKey, true)
+			r = r.WithContext(ctx)
+		}
+		next.ServeHTTP(w, r)
 	})
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,29 +1,36 @@
 package main
 
 import (
-    "net/http"
-    "github.com/justinas/alice"
+	"net/http"
+
+	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
-    mux := http.NewServeMux()
+	mux := http.NewServeMux()
 
-    fileServer := http.FileServer(http.Dir("./ui/static/"))
-    mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-    dynamic := alice.New(app.sessionManager.LoadAndSave)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
-    mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
-    mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
-    mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
-    mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
-    mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
-    mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
-    mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
-    mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
-    mux.Handle("POST /user/logout", dynamic.ThenFunc(app.userLogoutPost))
+	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
+	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
 
-    standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
-    
-    return standard.Then(mux)
+	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
+	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
+	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
+	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
+
+	mux.Handle("GET /about", dynamic.ThenFunc(app.about))
+
+	protected := dynamic.Append(app.requireAuthentcation)
+
+	mux.Handle("GET /snippet/create", protected.ThenFunc(app.snippetCreate))
+	mux.Handle("POST /snippet/create", protected.ThenFunc(app.snippetCreatePost))
+	mux.Handle("POST /user/logout", protected.ThenFunc(app.userLogoutPost))
+
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+
+	return standard.Then(mux)
 }
